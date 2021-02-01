8c80b9102fa6048d325e6fc19a8dc0d176153552
Ike Maldonado <maldonadoike@gmail.com>
2021-02-01 10:18:23 -0800

    Added all files from new Laravel project

---
+++ resources/views/portfolio.blade.php
@@ -0,0 +1,103 @@
+
+<div class="card  mb-4 card-hover">
+    <a href="pages/course-single.html" class="card-img-top"><img src="assets/images/course-graphql.jpg" alt=""
+            class="rounded-top card-img-top"></a>
+    <!-- Card Body -->
+    <div class="card-body">
+        <h4 class="mb-2 text-truncate-line-2 "><a href="pages/course-single.html" class="text-inherit">GraphQL:
+                introduction to graphQL for beginners</a></h4>
+        <!-- List -->
+        <ul class="mb-3 list-inline">
+            <li class="list-inline-item"><i class="far fa-clock mr-1"></i>2h 46m</li>
+            <li class="list-inline-item">
+                <svg class="mr-1 mt-n1" width="16" height="16" viewBox="0 0 16 16" fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <rect x="3" y="8" width="2" height="6" rx="1" fill="#754FFE"></rect>
+                    <rect x="7" y="5" width="2" height="9" rx="1" fill="#754FFE"></rect>
+                    <rect x="11" y="2" width="2" height="12" rx="1" fill="#754FFE"></rect>
+                </svg>
+                Advance
+            </li>
+        </ul>
+        <div class="lh-1">
+            <span>
+                <i class="mdi mdi-star text-warning mr-n1"></i>
+                <i class="mdi mdi-star text-warning mr-n1"></i>
+                <i class="mdi mdi-star text-warning mr-n1"></i>
+                <i class="mdi mdi-star text-warning mr-n1"></i>
+                <i class="mdi mdi-star text-warning"></i>
+            </span>
+            <span class="text-warning">4.5</span>
+            <span class="font-size-xs text-muted">(9,300)</span>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-12">
+        <!-- Card -->
+        <div class="card  mb-4 card-hover">
+            <a href="pages/course-single.html" class="card-img-top"><img src="assets/images/course-react.jpg" alt=""
+                    class="rounded-top card-img-top"></a>
+            <!-- Card Body -->
+            <div class="card-body">
+                <h4 class="mb-2 text-truncate-line-2 "><a href="pages/course-single.html" class="text-inherit">How to
+                        easily create a website with React</a></h4>
+                <!-- List -->
+                <ul class="mb-3 list-inline">
+                    <li class="list-inline-item"><i class="far fa-clock mr-1"></i>3h 56m</li>
+                    <li class="list-inline-item">
+                        <svg class="mr-1 mt-n1" width="16" height="16" viewBox="0 0 16 16" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <rect x="3" y="8" width="2" height="6" rx="1" fill="#754FFE"></rect>
+                            <rect x="7" y="5" width="2" height="9" rx="1" fill="#DBD8E9"></rect>
+                            <rect x="11" y="2" width="2" height="12" rx="1" fill="#DBD8E9"></rect>
+                        </svg>
+                        Beginner
+                    </li>
+                </ul>
+                <div class="lh-1">
+                    <span>
+                        <i class="mdi mdi-star text-warning mr-n1"></i>
+                        <i class="mdi mdi-star text-warning mr-n1"></i>
+                        <i class="mdi mdi-star text-warning mr-n1"></i>
+                        <i class="mdi mdi-star text-warning mr-n1"></i>
+                        <i class="mdi mdi-star text-warning"></i>
+                    </span>
+                    <span class="text-warning">4.5</span>
+                    <span class="font-size-xs text-muted">(7,700)</span>
+                </div>
+            </div>
+            <!-- Card Footer -->
+            <div class="card-footer">
+                <div class="row align-items-center no-gutters">
+                    <div class="col-auto">
+                        <img src="assets/images/avatar-1.jpg" class="rounded-circle avatar-xs" alt="">
+                    </div>
+                    <div class="col ml-2">
+                        <span>Morris Mccoy</span>
+                    </div>
+                    <div class="col-auto">
+                        <a href="#!" class="text-muted" data-toggle="tooltip" data-placement="top" title=""
+                            data-original-title="Add to Bookmarks">
+                            <i class="fe fe-bookmark  "></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><!-- Card Footer -->
+    <div class="card-footer">
+        <div class="row align-items-center no-gutters">
+            <div class="col-auto">
+                <img src="assets/images/avatar-2.jpg" class="rounded-circle avatar-xs" alt="">
+            </div>
+            <div class="col ml-2">
+                <span>Ted Hawkins</span>
+            </div>
+            <div class="col-auto">
+                <a href="#!" class="text-muted" data-toggle="tooltip" data-placement="top" title=""
+                    data-original-title="Add to Bookmarks">
+                    <i class="fe fe-bookmark  "></i>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
